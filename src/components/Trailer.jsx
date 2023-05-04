ee75f81a16a470943efad7f1e790667c8484d0f0
PaoloRiccardo <102251153+PaoloRiccardo@users.noreply.github.com>
2023-05-04 17:36:33 +0200

    Update Trailer.jsx

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -2,39 +2,34 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import YouTube from "react-youtube";
 
-
 function Trailer() {
   const params = useParams();
   const [videos, setVideos] = useState([]);
- 
-  
-  
+
   const fetchDetails = async () => {
     const data = await fetch(
       `https://api.themoviedb.org/3/movie/${params.name}/videos?api_key=${process.env.REACT_APP_API_KEY}`
-    )
-    const videos = await data.json()
-    console.log(videos.results.find(vid => vid.name === 'Official Trailer' || 'Teaser'));
-    setVideos(videos.results.find(vid => vid.name === 'Official Trailer' || 'Teaser'));
-    }
-  
+    );
+    const videos = await data.json();
+    console.log(
+      videos.results.find((vid) => vid.name === "Official Trailer" || "Teaser")
+    );
+    setVideos(
+      videos.results.find((vid) => vid.name === "Official Trailer" || "Teaser")
+    );
+  };
 
   useEffect(() => {
     fetchDetails();
     setVideos(videos);
   }, [params.name]);
 
-
-
-
-
   return (
     <div>
-    <h1 className="trailer-presentation"> Trailer:</h1>
-    <YouTube className="video-container" videoId={videos.key} />
+      <h1 className="trailer-presentation"> Trailer:</h1>
+      <YouTube className="video-container" videoId={videos.key} />
     </div>
   );
-};
-
+}
 
 export default Trailer;
