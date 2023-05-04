002acc20cc7c4bc75e2c6b3f9496bd2abe8e1b2a
PaoloRiccardo <102251153+PaoloRiccardo@users.noreply.github.com>
2023-05-04 17:36:13 +0200

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,29 @@
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Search () {
+export default function Search() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
-    const [input, setInput] = useState("")
-    const navigate = useNavigate();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/searched/" + input);
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        navigate("/searched/" + input)
-    };
-
-    return(
-        <div onSubmit={submitHandler}>
-            <div className="box-form">
-            <input className="input-box" onChange={(e) => setInput(e.target.value)} type="text" placeholder="Search a Movie" value={input}/>
-            <button className="button-form" onClick={submitHandler}>Search</button>
-            </div>
-
-        </div>
-    )
+  return (
+    <div onSubmit={submitHandler}>
+      <div className="box-form">
+        <input
+          className="input-box"
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder="Search a Movie"
+          value={input}
+        />
+        <button className="button-form" onClick={submitHandler}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
 }
