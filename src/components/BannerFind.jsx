f7bf438c1f141375b2358cf89091497cfcfaad68
PaoloRiccardo <102251153+PaoloRiccardo@users.noreply.github.com>
2023-05-04 17:33:31 +0200

    Update BannerFind.jsx

--- src/components/BannerFind.jsx
+++ src/components/BannerFind.jsx
@@ -1,26 +1,25 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-
 export const BannerFind = () => {
- 
-  const [text ] = useState('');
- 
-
+  const [text] = useState("");
 
   return (
     <section className="banner-category" id="home">
       <Container>
         <Row className="align-items-center">
           <Col xs={12}>
-           
-                {/*<span className="tagline">Category Movies</span>*/}
-                <h1>{`Find out all the details`} {text}</h1>
-                  <p>
-With "Vision" you will be able to discover many details about your films, TV series, or show business VIPs.</p>
+            {/*<span className="tagline">Category Movies</span>*/}
+            <h1>
+              {`Find out all the details`} {text}
+            </h1>
+            <p>
+              With "Vision" you will be able to discover many details about your
+              films, TV series, or show business VIPs.
+            </p>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
