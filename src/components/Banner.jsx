05cb47288b268831efd3b971fc393c5e004b7744
PaoloRiccardo <102251153+PaoloRiccardo@users.noreply.github.com>
2023-05-04 17:31:56 +0200

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,10 +5,10 @@ import headerImg from "../assets/img/header-img.png";
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Films", "TV Series", "and More" ];
+  const toRotate = ["Films", "TV Series", "and More"];
   const period = 2000;
 
   useEffect(() => {
@@ -16,49 +16,64 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
 
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-           
-                <span className="tagline">Welcome in Vision</span>
-                <h1 className="main-text">{`Hi! Here will find`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Films", "TV series", "And much more" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Vision is a simple web app to search or find information about movies.</p>
+            <span className="tagline">Welcome in Vision</span>
+            <h1 className="main-text">
+              {`Hi! Here will find`}{" "}
+              <span
+                className="txt-rotate"
+                dataPeriod="1000"
+                data-rotate='[ "Films", "TV series", "And much more" ]'
+              >
+                <span className="wrap">{text}</span>
+              </span>
+            </h1>
+            <p>
+              Vision is a simple web app to search or find information about
+              movies.
+            </p>
           </Col>
           <Col xs={12} md={6} xl={5}>
-                  <img className="goblin" src={headerImg} alt="Header Img"/>
+            <img className="goblin" src={headerImg} alt="Header Img" />
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
