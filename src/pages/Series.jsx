b0b2acd4baecd1109ac36cc38f0677bb075f0a02
PaoloRiccardo <102251153+PaoloRiccardo@users.noreply.github.com>
2023-05-04 17:30:13 +0200

    Update Series.jsx

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -4,43 +4,56 @@ import { NavBar } from "../components/NavBar";
 import { BannerCategory } from "../components/BannerCategory";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
-export default function Series () {
+export default function Series() {
   const [series, setSeries] = useState([]);
   let params = useParams();
 
   const getSeries = async (name) => {
     const data = await fetch(
-        `https://api.themoviedb.org/3/trending/${name}/day?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`); 
+      `https://api.themoviedb.org/3/trending/${name}/day?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
+    );
     const trending = await data.json();
     setSeries(trending.results);
   };
 
   useEffect(() => {
-   getSeries(params.genre);
+    getSeries(params.genre);
     console.log(params.genre);
   }, [params.genre]);
 
-  useEffect(() => {
-    document.title = "Vision | Category";
-  }, []);
-
   return (
     <div className="Category">
-    <NavBar/>
-    <BannerCategory/>
-    <div className="grid">
-      {series.map((item) => {
-        return(
-          <motion.div className="card-movie" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} key={item.id}>
-              <Link to={`/seriedet/${item.id}`}>   
-            <img className="movies-tv" src={`https://image.tmdb.org/t/p/w500` + item.backdrop_path} alt="" />
-            </Link>
-            <h4 className="category-title">{item.title}{item.name}</h4>
-          </motion.div>
-        )
-      })}
-    </div>
+      <Helmet>
+        <title>Vision | Category </title>
+      </Helmet>
+      <NavBar />
+      <BannerCategory />
+      <div className="grid">
+        {series.map((item) => {
+          return (
+            <motion.div
+              className="card-movie"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              key={item.id}
+            >
+              <Link to={`/seriedet/${item.id}`}>
+                <img
+                  className="movies-tv"
+                  src={`https://image.tmdb.org/t/p/w500` + item.backdrop_path}
+                  alt=""
+                />
+              </Link>
+              <h4 className="category-title">
+                {item.title}
+                {item.name}
+              </h4>
+            </motion.div>
+          );
+        })}
+      </div>
     </div>
   );
 }
